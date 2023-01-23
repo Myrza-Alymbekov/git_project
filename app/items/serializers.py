e7163a686633ac24d5337ae75c089e31a0e876d2
DadyaChel <kvvvs04@gmail.com>
2023-01-23 10:42:55 +0300

    Item

--- app/items/serializers.py
+++ app/items/serializers.py
@@ -2,6 +2,12 @@ from rest_framework import serializers
 from app.items.models import Item
 
 
+class ItemDetailSerialiser(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        exclude = ['item']
+
+
 class ItemSerializer(serializers.Serializer):
     fields = '__all__'
 
